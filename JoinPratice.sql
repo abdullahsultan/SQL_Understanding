6b1403887015e4ea579ccb367d3b35ae015dc173
M.Abdullah Sultan <abdullahsultan448@gmail.com>
2024-07-15 22:33:23 +0400

    Cross Joins

--- JoinPratice.sql
+++ JoinPratice.sql
@@ -29,3 +29,29 @@ FROM
     employees AS emp
         LEFT JOIN
     dept_manager AS dm ON emp.emp_no = dm.emp_no;
+    
+-- CROSS JOIN
+
+SELECT 
+    dm.*, dep.*
+FROM
+    dept_manager AS dm
+        CROSS JOIN
+    departments AS dep
+WHERE
+    dep.dept_no = 'd009'
+ORDER BY dep.dept_no;
+
+select @@global.sql_mode;
+set @@global.sql_mode := replace(@@global.sql_mode, 'ONLY_FULL_GROUP_BY', '');
+
+SELECT 
+    e.*, d.*
+FROM
+    employees AS e
+        CROSS JOIN
+    departments AS d
+WHERE
+    e.emp_no < 10011
+ORDER BY e.emp_no , d.dept_name
+
