4936bdba1797f78575f4f5dab86cef7d2f1246db
M.Abdullah Sultan <abdullahsultan448@gmail.com>
2024-07-17 21:12:58 +0400

    Lecture 209 for Nested queries practice

--- NestedQueris.sql
+++ NestedQueris.sql
@@ -20,9 +20,28 @@ SELECT * FROM employees
 INNER JOIN titles ON titles.emp_no = employees.emp_no WHERE titles.title = "Assistant Engineer" ORDER BY employees.emp_no ASC;
 
 
+-- LECTURE 209
+SELECT A.* FROM(
+SELECT employees.emp_no AS employee_id, departments.dept_no AS department_code,  
+(SELECT emp_no FROM dept_manager WHERE dept_manager.emp_no = 110022) AS manager_ID
+FROM employees
+INNER JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
+INNER JOIN departments ON departments.dept_no = dept_emp.dept_no WHERE employees.emp_no BETWEEN 10000 AND 10020) AS A
+UNION
+SELECT B.* FROM(
+SELECT employees.emp_no AS employee_id, departments.dept_no AS department_code,  
+(SELECT emp_no FROM dept_manager WHERE dept_manager.emp_no = 110022) AS manager_ID
+FROM employees
+INNER JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
+INNER JOIN departments ON departments.dept_no = dept_emp.dept_no WHERE employees.emp_no BETWEEN 10021 AND 10040) AS B
+ORDER BY employee_id ASC;
+
+
 -- Exercise 210
 CREATE TABLE emp_manager(
 	emp_no INT(11) NOT NULL,
     dept_no CHAR(4),
     manager_no INT(11) NOT NULL
 );
+
+
