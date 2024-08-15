facee955f33fc0fff6b47d1e8628b68f53d4b23b
M.Abdullah Sultan <abdullahsultan448@gmail.com>
2024-08-15 23:44:38 +0400

    Lecture 320

--- TableauAndSQL.sql
+++ TableauAndSQL.sql
@@ -10,3 +10,26 @@ JOIN t_dept_manager ON t_employees.emp_no = t_employees.emp_no
 GROUP BY calendar_year, gender 
 HAVING calendar_year >= 1990 ORDER BY calendar_year
 ;
+
+SELECT 
+YEAR(t_dept_emp.from_date) AS calendar_year,
+t_employees.emp_no,
+t_dept_emp.dept_no,
+t_dept_emp.from_date,
+t_dept_emp.to_date,
+t_departments.dept_name,
+t_employees.birth_date,
+t_employees.first_name,
+t_employees.last_name,
+t_employees.gender,
+t_employees.hire_date
+FROM
+	t_employees
+JOIN
+	t_dept_emp ON t_employees.emp_no = t_dept_emp.emp_no
+JOIN
+	t_departments ON t_departments.dept_no = t_dept_emp.dept_no
+ORDER BY calendar_year, t_employees.emp_no;
+
+
+
